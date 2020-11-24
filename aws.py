11d0f68b82c5db15a27ac66a0780cf68374845ce
Ronaldo Afonso <ronaldo@ronaldoafonso.com.br>
2020-11-24 17:40:28 -0300

    Refactor the aws module.

--- aws.py
+++ aws.py
@@ -2,43 +2,36 @@
 import boto3
 
 
-def Client():
-    return boto3.client('ec2')
+class Client:
+
+    def __init__(self):
+        self.client = boto3.client('ec2')
+
+    def get_id(self):
+        return self.resource_id
+
+    def tag(self, key, value):
+        self.client.create_tags(
+            Resources=[self.get_id()],
+            Tags=[{'Key': key, 'Value': value}]
+        )
 
 
-class Vpc:
-
-    def __init__(self, client):
-        self.client = client
+class Vpc(Client):
 
     def create(self, cidr_block):
         vpc = self.client.create_vpc(CidrBlock=cidr_block)
-        self.vpc_id = vpc['Vpc']['VpcId']
+        self.resource_id = vpc['Vpc']['VpcId']
 
     def delete(self):
-        return self.client.delete_vpc(VpcId=self.vpc_id)
-
-    def tag(self, key, value):
-        self.client.create_tags(
-            Resources=[self.vpc_id],
-            Tags=[{'Key': key, 'Value': value}]
-        )
+        return self.client.delete_vpc(VpcId=self.get_id())
 
 
-class Igw:
-
-    def __init__(self, client):
-        self.client = client
+class Igw(Client):
 
     def create(self):
         igw = self.client.create_internet_gateway()
-        self.igw_id = igw['InternetGateway']['InternetGatewayId']
+        self.resource_id = igw['InternetGateway']['InternetGatewayId']
 
     def delete(self):
-        self.client.delete_internet_gateway(InternetGatewayId=self.igw_id)
-
-    def tag(self, key, value):
-        self.client.create_tags(
-            Resources=[self.igw_id],
-            Tags=[{'Key': key, 'Value': value}]
-        )
+        self.client.delete_internet_gateway(InternetGatewayId=self.get_id())
