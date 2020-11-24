48e75820946d612fc75f977e65fe9b7a591116e4
Ronaldo Afonso <ronaldo@ronaldoafonso.com.br>
2020-11-24 19:17:03 -0300

    Create a class for Route Tables and Associations between Route Tables and Subnets.

---
+++ routetable.py
@@ -0,0 +1,34 @@
+
+from aws import Aws
+
+
+class RouteTable(Aws):
+
+    def create(self, vpc_id):
+        routetable = self.aws.create_route_table(VpcId=vpc_id)
+        self.resource_id = routetable['RouteTable']['RouteTableId']
+
+    def add_route(self, igw_id, dest_cidr_block):
+        self.aws.create_route(
+            RouteTableId=self.get_id(),
+            GatewayId=igw_id,
+            DestinationCidrBlock=dest_cidr_block
+        )
+
+    def delete(self):
+        self.aws.delete_route_table(RouteTableId=self.get_id())
+
+
+class Association(Aws):
+
+    def assoc_route_table_with_subnet(self, routetable_id, subnet_id):
+        association = self.aws.associate_route_table(
+            RouteTableId=routetable_id,
+            SubnetId=subnet_id
+        )
+        self.resource_id = association['AssociationId']
+
+    def disassoc_route_table_with_subnet(self):
+        self.aws.disassociate_route_table(
+                AssociationId=self.get_id()
+        )
