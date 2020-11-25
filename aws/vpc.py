5598607f3cbb85348f10c0a8c31beea100c8ea76
Ronaldo Afonso <ronaldo@ronaldoafonso.com.br>
2020-11-25 18:42:28 -0300

    Refactor VPC class so the CIDR block must be provided at object creation time.

--- aws/vpc.py
+++ aws/vpc.py
@@ -4,9 +4,13 @@ from .aws import Aws
 
 class Vpc(Aws):
 
-    def create(self, cidr_block):
-        vpc = self.aws.create_vpc(CidrBlock=cidr_block)
+    def __init__(self, cidr_block):
+        self.cidr_block = cidr_block
+        super().__init__()
+
+    def create(self):
+        vpc = self.aws.create_vpc(CidrBlock=self.cidr_block)
         self.resource_id = vpc['Vpc']['VpcId']
 
     def delete(self):
-        return self.aws.delete_vpc(VpcId=self.get_id())
+        self.aws.delete_vpc(VpcId=self.get_id())
